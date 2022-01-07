d61be0ae8d913244da1210eaba71e78215a0195e
moerwald <andreas.mewald@gmail.com>
2022-01-07 17:19:19 +0100

    Added user control to display note

---
+++ src/EvernoteClone/EvernoteClone/Views/CustomControls/DisplayNote.xaml.cs
@@ -0,0 +1,50 @@
+﻿using EvernoteClone.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EvernoteClone.Views.CustomControls
+{
+    /// <summary>
+    /// Interaction logic for DisplayNote.xaml
+    /// </summary>
+    public partial class DisplayNote : UserControl
+    {
+
+
+        public Note Note
+        {
+            get { return (Note)GetValue(NoteProperty); }
+            set { SetValue(NoteProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Note.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NoteProperty =
+            DependencyProperty.Register("Note", typeof(Note), typeof(DisplayNote), new PropertyMetadata(null, SetValue ));
+
+        private static void SetValue (DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is DisplayNote displayNote )
+            {
+                displayNote.DataContext = displayNote.Note;
+            }
+        }
+
+
+        public DisplayNote()
+        {
+            InitializeComponent();
+        }
+    }
+}
